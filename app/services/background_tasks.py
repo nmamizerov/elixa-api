b62049871838f132470f1a9c3e9cb335d9b3d9f9
Мамизеров Никита <nmamizerov@gmail.com>
2025-08-15 17:44:37 +0300

    add google config from env

--- app/services/background_tasks.py
+++ app/services/background_tasks.py
@@ -34,80 +34,58 @@ class BackgroundTaskService:
 
         self.running_tasks.add(task_id)
 
-        try:
-            # Получаем сессию базы данных
-            async with SessionLocal() as session:
-                try:
-                    report_repo = ReportRepository(session)
-                    report_service = ReportService(
-                        report_repo,
-                        yandex_metrika_integration=yandex_metrika_integration,
-                        google_analytics_integration=google_analytics_integration,
-                    )
-
-                    # Получаем отчет из базы
-                    report = await report_repo.get_report_by_id(report_id)
-                    if not report:
-                        logger.error(f"Report {report_id} not found")
-                        return
-
-                    # Используем компанию из relationship отчета
-                    company = report.company
-                    if not company:
-                        logger.error(f"Company {report.company_id} not found")
-                        await report_repo.update_report_status(
-                            report_id, StatusEnum.failed
-                        )
-                        return
-
-                    # Генерируем данные отчета
-                    report_data = await report_service._generate_report_data(
-                        report, sources_list=sources_list
-                    )
-
-                    if report_data:
-                        # Сохраняем отчет в Excel файл
-                        file_name = await save_report_to_excel(
-                            report_data, f"report_{report.source}"
-                        )
-
-                        # Обновляем статус отчета
-                        await report_repo.update_report_file(report.id, file_name)
-                        await report_repo.update_report_status(
-                            report.id, StatusEnum.finish
-                        )
-
-                        logger.info(f"Report {report_id} generated successfully")
-
-                        if not report.is_compared:
-                            self.start_conclusion_generation(report_id)
-                    else:
-                        # Ошибка при генерации данных
-                        await report_repo.update_report_status(
-                            report.id, StatusEnum.failed
-                        )
-                        logger.error(f"Failed to generate data for report {report_id}")
-
-                except Exception as e:
-                    logger.error(f"Error processing report {report_id}: {str(e)}")
-                    # Обновляем статус на ошибку
-                    async with SessionLocal() as error_session:
-                        try:
-                            error_repo = ReportRepository(error_session)
-                            await error_repo.update_report_status(
-                                report_id, StatusEnum.failed
-                            )
-                        except Exception as inner_e:
-                            logger.error(
-                                f"Failed to update report status: {str(inner_e)}"
-                            )
-
-        except Exception as e:
-            logger.error(
-                f"Critical error in background task for report {report_id}: {str(e)}"
+        # try:
+        # Получаем сессию базы данных
+        async with SessionLocal() as session:
+            report_repo = ReportRepository(session)
+            report_service = ReportService(
+                report_repo,
+                yandex_metrika_integration=yandex_metrika_integration,
+                google_analytics_integration=google_analytics_integration,
             )
-        finally:
-            self.running_tasks.discard(task_id)
+
+            # Получаем отчет из базы
+            report = await report_repo.get_report_by_id(report_id)
+            if not report:
+                logger.error(f"Report {report_id} not found")
+                return
+
+            # Используем компанию из relationship отчета
+            company = report.company
+            if not company:
+                logger.error(f"Company {report.company_id} not found")
+                await report_repo.update_report_status(report_id, StatusEnum.failed)
+                return
+
+            # Генерируем данные отчета
+            report_data = await report_service._generate_report_data(
+                report, sources_list=sources_list
+            )
+
+            if report_data:
+                # Сохраняем отчет в Excel файл
+                file_name = await save_report_to_excel(
+                    report_data, f"report_{report.source}"
+                )
+
+                # Обновляем статус отчета
+                await report_repo.update_report_file(report.id, file_name)
+                await report_repo.update_report_status(report.id, StatusEnum.finish)
+
+                logger.info(f"Report {report_id} generated successfully")
+
+                if not report.is_compared:
+                    self.start_conclusion_generation(report_id)
+            else:
+                # Ошибка при генерации данных
+                await report_repo.update_report_status(report.id, StatusEnum.failed)
+                logger.error(f"Failed to generate data for report {report_id}")
+        # except Exception as e:
+        #     logger.error(
+        #         f"Critical error in background task for report {report_id}: {str(e)}"
+        #     )
+        # finally:
+        self.running_tasks.discard(task_id)
 
     def start_report_generation(
         self,
